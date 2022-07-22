4912a2c91e21f8e66441804f15ce33c2edde76ca
Andreas Kaeberlein <andreas.kaeberlein@siemens.com>
2022-07-22 11:22:58 +0200

    add prototypes

---
+++ spi_flash_cb.c
@@ -0,0 +1,27 @@
+/***********************************************************************
+ @copyright     : Siemens AG
+ @license       : Siemens Inner Source License 1.4
+ @author        : Andreas Kaeberlein
+ @address       : Clemens-Winkler-Strasse 3, 09116 Chemnitz
+
+ @maintainer    : Andreas Kaeberlein
+ @telephone     : +49 371 4810-2108
+ @email         : andreas.kaeberlein@siemens.com
+
+ @file          : spi_flash_cb.h
+ @date          : July 22, 2022
+
+ @brief         : SPI FLash Circular Buffer
+                  Driver for handling Circular Buffers in SPI Flashes
+***********************************************************************/
+
+
+
+/** Includes **/
+/* Standard libs */
+#include <stdint.h>     // defines fixed data types: int8_t...
+#include <stddef.h>     // various variable types and macros: size_t, offsetof, NULL, ...
+#include <string.h>     // string operation: memset, memcpy
+/* Self */
+#include "spi_flash_cb.h"
+
---
+++ spi_flash_cb.h
@@ -0,0 +1,38 @@
+/***********************************************************************
+ @copyright     : Siemens AG
+ @license       : Siemens Inner Source License 1.4
+ @author        : Andreas Kaeberlein
+ @address       : Clemens-Winkler-Strasse 3, 09116 Chemnitz
+
+ @maintainer    : Andreas Kaeberlein
+ @telephone     : +49 371 4810-2108
+ @email         : andreas.kaeberlein@siemens.com
+
+ @file          : spi_flash_cb.h
+ @date          : July 22, 2022
+
+ @brief         : SPI FLash Circular Buffer
+                  Driver for handling Circular Buffers in SPI Flashes
+***********************************************************************/
+
+
+// Define Guard
+#ifndef __SPI_FLASh_CB_H
+#define __SPI_FLASh_CB_H
+
+
+/* C++ compatibility */
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif // __SPI_FLASh_CB_H
