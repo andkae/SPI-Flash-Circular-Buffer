14a3f0edb0ea080f12641d40abc1912d61b2eece
Andreas Kaeberlein <andreas.kaeberlein@siemens.com>
2022-07-25 16:20:50 +0200

    add: spi_flash_cb_init, spi_flash_cb_add

--- spi_flash_cb.c
+++ spi_flash_cb.c
@@ -23,5 +23,98 @@
 #include <stddef.h>     // various variable types and macros: size_t, offsetof, NULL, ...
 #include <string.h>     // string operation: memset, memcpy
 /* Self */
-#include "spi_flash_cb.h"
+#include "spi_flash_cb_hal.h"	// supported spi flashes
+#include "spi_flash_cb.h"		// function prototypes
+
+
+
+/**
+ *  spi_flash_cb_ceildivide2
+ *    divides to the power 2
+ *    round always up
+ */
+uint32_t spi_flash_cb_ceildivide(uint32_t dividend, uint32_t divisor)
+{   
+    if ( dividend != 0 ) {
+        return ( 1 + ( (dividend-1) / divisor ) );
+    };
+    return ( ( dividend + divisor - 1) / divisor );
+}
+
+
+/**
+ *  spi_flash_cb_max
+ *    get maximum of number
+ */
+uint32_t spi_flash_cb_max(uint16_t val1, uint16_t val2)
+{
+    if ( val1 > val2 ) {
+		return val1;
+	}
+    return val2;
+}
+
+
+/**
+ *  spi_flash_cb_init
+ *    initializes handle
+ */
+int spi_flash_cb_init (spi_flash_cb *self, uint8_t flashType, void *cbMem, uint8_t numCbs)
+{
+    /* check if provided flash type is valid */
+    if ( flashType > ((sizeof(SPI_FLASH_CB_TYPES)/sizeof(SPI_FLASH_CB_TYPES[0]))-1) ) {
+		return 1;
+	}
+    /* set up list of flash circular buffers */
+    self->uint8FlashType = flashType;
+    self->uint8NumCbs = numCbs;
+    self->ptrCbs = cbMem;
+    /* init list */
+    for ( uint8_t i=0; i<self->uint8NumCbs; i++ ) {
+		((spi_flash_cb_elem*) self->ptrCbs)[i].uint8Used = 0;
+		((spi_flash_cb_elem*) self->ptrCbs)[i].uint8Init = 0;
+	}
+	/* normal end */
+	return 0;
+}
+
+
+
+/**
+ *  spi_flash_cb_add
+ *    add circular buffer entry
+ */
+int spi_flash_cb_add (spi_flash_cb *self, uint32_t magicNum, uint16_t elemSizeByte, uint16_t numElems)
+{	
+	/** help variables **/
+	const uint8_t		uint8NumHeadBytes = 2*sizeof(((spi_flash_cb_elem *)0)->uint32MagicNum) + sizeof(((spi_flash_cb_elem *)0)->uint32HighSeriesNum);
+	spi_flash_cb_elem*	cb_elem = self->ptrCbs;
+	uint8_t				uint8FreeCbEntry;
+	uint32_t			uint32StartSector;
+	uint32_t			uint32NumSectors;
+	
+	/* check for free Slot number */
+	uint32StartSector = 0;
+	for ( uint8FreeCbEntry = 0; uint8FreeCbEntry < (self->uint8NumCbs); uint8FreeCbEntry++ ) {
+		if ( 0 == cb_elem[uint8FreeCbEntry].uint8Used ) {
+			break;
+		} else {
+			uint32StartSector = cb_elem[uint8FreeCbEntry].uint32StopSector + 1;	// next sector is used 
+		}
+	}
+	if ( uint8FreeCbEntry == (self->uint8NumCbs) ) {
+		return 1;	// no free circular buffer slots
+	}	
+	/* prepare slot */
+	cb_elem[uint8FreeCbEntry].uint8Used = 1;	// occupied
+	cb_elem[uint8FreeCbEntry].uint16NumPagesPerElem = spi_flash_cb_ceildivide(elemSizeByte+uint8NumHeadBytes, SPI_FLASH_CB_TYPES[self->uint8FlashType].uint32FlashTopoPageSizeByte);	// calculate in multiple of pages
+	cb_elem[uint8FreeCbEntry].uint32StartSector = uint32StartSector;
+	uint32NumSectors = spi_flash_cb_max(2, spi_flash_cb_ceildivide(numElems*cb_elem[uint8FreeCbEntry].uint16NumPagesPerElem, SPI_FLASH_CB_TYPES[self->uint8FlashType].uint32FlashTopoSectorSizeByte));
+	cb_elem[uint8FreeCbEntry].uint32StopSector = cb_elem[uint8FreeCbEntry].uint32StartSector+uint32NumSectors-1;
+	/* succesfull */
+	return 0;
+}
+
+
+
 
--- spi_flash_cb.h
+++ spi_flash_cb.h
@@ -17,8 +17,8 @@
 
 
 // Define Guard
-#ifndef __SPI_FLASh_CB_H
-#define __SPI_FLASh_CB_H
+#ifndef __SPI_FLASH_CB_H
+#define __SPI_FLASH_CB_H
 
 
 /* C++ compatibility */
@@ -28,6 +28,77 @@ extern "C"
 #endif // __cplusplus
 
 
+typedef struct spi_flash_cb_elem
+{
+    uint8_t		uint8Used : 1;				/**< Entry is used */
+    uint8_t		uint8Init : 1;				/**< Data structure initialized */
+    uint32_t	uint32MagicNum;				/**< Magic Number for marking valid block */
+    uint32_t	uint32HighSeriesNum;		/**< Highest Number in series number field */
+    uint32_t	uint32StartSector;			/**< Start Sector of the Circular Buffer */
+    uint32_t	uint32StopSector;			/**< Stop Sector. At least two sectors are required, otherwise will sector erase delete complete buffer */
+    uint32_t	uint32StartPageNewEntry;	/**< Start Page number of next entry */
+    uint16_t	uint16NumPagesPerElem;		/**< Number of pages per element */
+} __attribute__((packed)) spi_flash_cb_elem;
+
+
+
+/**
+ *  @typedef spi_flash_cb
+ *
+ *  @brief  circular buffer
+ *
+ *  handle for circular buffer
+ *
+ *  @since  July 22, 2022
+ *  @author Andreas Kaeberlein
+ */
+typedef struct spi_flash_cb
+{
+    uint8_t		uint8FlashType;	/**< pointer to flashtype. see #t_spi_flash_cb_type_descr */
+    uint8_t		uint8NumCbs;	/**< number of circular buffers */
+    void*		ptrCbs;			/**< List with flash circular buffers */
+} spi_flash_cb;
+
+
+
+/**
+ *  @brief init
+ *
+ *  initializes flash circular buffer
+ *
+ *  @param[in,out]  self                handle
+ *  @param[in]      flashType           index of flash type in #SPI_FLASH_CB_TYPES
+ *  @param[in,out]  *cbMem              pointer to allocated memory for circulat buffer list
+ *  @param[in]      numCbs              number of maximum allowed circular buffers
+ *  @return         int                 state
+ *  @retval         0                   OKAY
+ *  @retval         1                   Invalid Flash Type
+ *  @since          2022-07-25
+ *  @author         Andreas Kaeberlein
+ */
+int spi_flash_cb_init (spi_flash_cb *self, uint8_t flashType, void *cbMem, uint8_t numCbs);
+
+
+/**
+ *  @brief add
+ *
+ *  add entry to flash circular buffer parition table
+ *
+ *  @param[in,out]  self                handle
+ *  @param[in]      magicNum            Magic Number for marking enties valid, should differ between different Circular buffer entries
+ *  @param[in]      elemSizeByte        Size of one element in the circular buffer in byte
+ *  @param[in]      numElems            minimal number of elements in the circular buffer, through need of sector erase and not deleting all data can be this number higher then requested
+ *  @return         int                 state
+ *  @retval         0                   OKAY
+ *  @retval         1                   No free circular buffer slots, allocate more static memory
+ *  @since          2022-07-25
+ *  @author         Andreas Kaeberlein
+ */
+int spi_flash_cb_add (spi_flash_cb *self, uint32_t magicNum, uint16_t elemSizeByte, uint16_t numElems);
+
+
+
+
 
 
 #ifdef __cplusplus
@@ -35,4 +106,4 @@ extern "C"
 #endif // __cplusplus
 
 
-#endif // __SPI_FLASh_CB_H
+#endif // __SPI_FLASH_CB_H
